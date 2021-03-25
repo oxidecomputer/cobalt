c4b6244a04104bf638d2e8c90adee9451cbec052
Arjen Roodselaar <arjen@oxide.computer>
2021-03-25 00:33:51 -0700

    Update copyright header

--- tools/site_cobble/bluescan.py
+++ tools/site_cobble/bluescan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2020 Oxide Computer Company
+# Copyright 2021 Oxide Computer Company
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
--- tools/site_cobble/nextpnr.py
+++ tools/site_cobble/nextpnr.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Oxide Computer Company
+# Copyright 2021 Oxide Computer Company
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
