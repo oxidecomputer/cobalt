e5012a72564ea0149dd9e351780a16acd736769b
Nathanael Huffman <nathanael@oxidecomputer.com>
2022-04-07 12:26:18 -0500

    Clean up

--- tools/site_cobble/rdl.py
+++ tools/site_cobble/rdl.py
@@ -21,12 +21,10 @@ KEYS = frozenset([RDL_SCRIPT, RDL_ODIR])
 _ver_keys = frozenset([RDL_SCRIPT.name])
 
 
+# Helper function to fix up the case into a bsv-standards compatible
+# filename.
 def to_camel_case(template_string, uppercamel=False):
     return inflection.camelize(template_string, uppercase_first_letter=uppercamel)
-
-
-def to_snake_case(template_string):
-    return inflection.underscore(template_string)
 
 
 @target_def
@@ -37,22 +35,37 @@ def rdl(package, name, *,
         using: Delta = {}):
     
     def mkusing(ctx):
+        # get absolute path for the sources
         sources_i = ctx.rewrite_sources(sources)
         env = ctx.env.subset_require(_ver_keys)
+        # Determine output directory since we need to output some files here
+        # TODO: deal with additional outputs.
         out_dir = package.outpath(env)
         p_env = env.derive({
             RDL_ODIR.name:out_dir,
         })
         in_name = Path(sources_i[0]).stem
         bsv_name = f'{to_camel_case(in_name.lower(), uppercamel=True)}.bsv'
-        output = package.outpath(env, bsv_name)
+        bsv_pkg = package.outpath(env, bsv_name)
+        reg_name = in_name +'.html'
+        reg_map = package.outpath(env, reg_name)
+        json_name = in_name + '.json'
+        reg_json = package.outpath(env, json_name)
         product = cobble.target.Product(
             env = p_env,
             inputs = sources_i,
-            outputs = [output],
+            outputs = ([bsv_pkg], [reg_json, reg_map]),
             rule = 'rdl_script')
 
-        product.expose(path = output, name = bsv_name)
+        product.expose(path = bsv_pkg, name = bsv_name)
+
+        product.expose(path = reg_map, name = reg_name)
+        product.symlink(target = reg_map, source = package.linkpath(reg_name))
+
+        product.expose(path = reg_json, name = json_name)
+        product.symlink(target = reg_json, source = package.linkpath(json_name))
+       
+
         return (using, [product])
 
     return cobble.target.Target(
