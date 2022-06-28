faf4d3fb05b5fa5c8e4aa9e811be295c3333ebcc
Nathanael Huffman <nathanaelhuffman@users.noreply.github.com>
2022-06-28 15:18:33 -0500

    Add hack for making ninja always generate up-to-date sha (#28)
    Add defaulValue for generated registers.

--- tools/site_cobble/gen_git_version_bsv.py
+++ tools/site_cobble/gen_git_version_bsv.py
@@ -1,5 +1,5 @@
 import argparse
-
+import json
 from pathlib import Path
 from string import Template
 
@@ -7,8 +7,8 @@ from string import Template
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
-parser.add_argument(dest='version_code', help="Integer version code")
-parser.add_argument(dest='short_sha', help="8 character short SHA")
+# parser.add_argument(dest='version_code', help="Integer version code")
+# parser.add_argument(dest='short_sha', help="8 character short SHA")
 parser.add_argument(dest='output_filename', help="string for bluespec package name")
 
 template = Template("""
@@ -27,8 +27,16 @@ endpackage
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    version = f'{int(args.version_code, 0):x}'
-    sha = f'{int(args.short_sha, 16):x}'
+    # version = f'{int(args.version_code, 0):x}'
+    # sha = f'{int(args.short_sha, 16):x}'
+
+    with open('git_sha_hack.json', 'r') as infile:
+        f = json.load(infile)
+
+    my_code = f['code']
+    my_sha = f['sha'][:8]
+    version = f'{int(my_code, 0):x}'
+    sha = f'{int(my_sha, 16):x}'
 
     package_name = str(Path(args.output_filename).stem)
 
