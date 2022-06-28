436bb6dc39ed60dc2edb49694a846afd2e6d7615
Nathanael Huffman <nathanael@oxidecomputer.com>
2022-06-28 09:07:58 -0500

    Add hack for making ninja always generate up-to-date sha

--- tools/site_cobble/bsv_fpga_version.py
+++ tools/site_cobble/bsv_fpga_version.py
@@ -4,6 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import json
+import pathlib
 import cobble.env
 from cobble.plugin import *
 from cobble.git_version import *
@@ -16,10 +18,37 @@ KEYS = frozenset([GEN_GIT_VERSION_BSV])
 _ver_keys = frozenset([GEN_GIT_VERSION_BSV.name, GIT_VERSION_CODE.name, GIT_VERSION_REV_SHA1_SHORT.name])
 
 
+# This is a *super* ugly hack to force ninja to to do the right thing
+# We're going to dump out the current sha + stuff into a json file *only* if it needs to be updated, and we'll make this file
+# an implicit dependency to make this run when necessary.
+# Cobble envs aren't available at this time since we don't have an environment and we can't rely on ninja unless we have a file
+# that changes so we're making a file that changes.
+config = GitVersionerConfig('git', 'main', '..', 1000, 48, "", "HEAD")
+versioner = GitVersioner(config)
+out = {
+    'sha': versioner.sha1,
+    'code': str(versioner.revision)
+}
+test_in = {
+    'sha': "",
+    'code': ""
+}
+
+try:
+    with open('git_sha_hack.json', 'r') as infile:
+        test_in = json.load(infile)
+except:
+    pass
+
+if (test_in['sha'] != out['sha']) or (test_in['code'] != out['code']):
+    print("Git sha changed, updating file")
+    with open('git_sha_hack.json', 'w') as outfile:
+      json.dump(out, outfile)
+
 @target_def
 def bsv_fpga_version(package, name, *,
         deps = [],
-        sources = [],
+        sources = ['./git_sha_hack.json'],
         local: Delta = {},
         using: Delta = {}):
     def mkusing(ctx):
@@ -46,7 +75,7 @@ def bsv_fpga_version(package, name, *,
 
 ninja_rules = {
     'gen_git_version_bsv': {
-        'command': ' python3 $gen_git_version_bsv $git_version_code $git_version_rev_sha1_short $out > $out',
+        'command': ' python3 $gen_git_version_bsv $out > $out',
         'description': 'gen_git_version_bsv.py $out',
     }
 }
